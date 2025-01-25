8e9d315f264ced8aeddbb5b0ae30ebddfbac165b
Dev-NayanBiswas <dev.nayan.biswas@gmail.com>
2025-01-26 04:13:21 +0600

    transaction history and add students curd operations implimented

--- Routers/reviews.js
+++ Routers/reviews.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const {reviews, meals, ObjectId} = require("../Config/dataBase.js");
 const CustomErrors = require('../Errors/CustomErrors.js');
-const {verifyToken} = require('../middlewares/verifications.js');
+const {verifyToken, verifyStudent} = require('../middlewares/verifications.js');
 const router = express.Router();
 
 
@@ -37,6 +37,53 @@ router.route("/")
     res.status(200).send({message:'Review Added Successfully', result:result})
     }catch(error){
         next(new CustomErrors("Error in adding Reviews", 500))
+    }
+})
+
+router.route("/studentReviews/:email")
+.patch(verifyToken, verifyStudent,async(req,res,next)=>{
+    const {email} = req.params;
+    const {reviewID,comment,rating, mealID} = req.body;
+    console.log(mealID)
+    const options = {$set:{comment,rating}};
+    try{
+        const result = await reviews.updateOne({_id:new ObjectId(reviewID)}, options,{upsert:true});
+        if(result.modifiedCount > 0){
+            await meals.updateOne({_id : new ObjectId(mealID)},{$inc:{rating:- rating}})
+        };
+    res.status(200).send({message:"Review Updated", result:result})
+    }catch(error){
+        next(new CustomErrors("Error in Updating Review", 500))
+    }
+})
+.delete(verifyToken, verifyStudent,async(req,res,next)=>{
+    const {email} = req.params;
+    const {reviewID, mealID, myRating} = req.query;
+    // console.log(reviewID,mealID);
+    // console.log(email, "from Delete Reviews");
+    const result = await reviews.deleteOne({_id:new ObjectId(reviewID)});
+    if(result.deletedCount){
+        await meals.updateOne({_id:new ObjectId(mealID)},{$inc:{reviewCount: - 1, rating: - myRating}});
+    }
+    res.status(200).send({message:"Review Deleted", result:result})
+})
+.get(verifyToken, verifyStudent,async(req,res,next)=>{
+    const {email} = req.params;
+    try{
+        const userReviews = await reviews.find({email:email}).toArray();
+    const reviewIdAndObjectID = userReviews.map(({mealID, _id, comment, rating})=>({_id:new ObjectId(mealID), reviewID:_id, comment:comment, myRating:rating}));
+    const onlyIDs = reviewIdAndObjectID.map(({_id})=>_id);
+
+    const reviewMeals = await meals.find({_id:{$in:onlyIDs}}).toArray();
+
+    const reviewsWithReviewID = reviewMeals.map((meal)=>{
+        const {reviewID,comment,myRating} = reviewIdAndObjectID.find(
+            (rMeals)=>rMeals._id.equals(meal._id));
+        return {...meal,reviewID, comment,myRating}
+    })
+    res.status(200).send({message:"Reviews fetched from Database", result:reviewsWithReviewID})
+    }catch(error){
+        next(new CustomErrors("Error in Fetching My Reviews", 500))
     }
 })
 
--- Routers/transactions.js
+++ Routers/transactions.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { transactions, ObjectId } = require("../Config/dataBase.js");
 const CustomErrors = require("../Errors/CustomErrors.js");
-const {verifyToken} = require('../middlewares/verifications.js')
+const {verifyToken, verifyStudent} = require('../middlewares/verifications.js')
 const router = express.Router();
 
 
@@ -14,6 +14,17 @@ router.route("/")
     }catch(error){
         next(new CustomErrors("Error in update Transaction history"))
     }
+})
+router.route("/studentTransactions/:email")
+.get(verifyToken, verifyStudent,async(req,res,next)=>{
+    const {email} = req.params;
+    try{
+        const studentTransactions = await transactions.find({email}).toArray();
+    res.status(200).send({message:"Transaction History", result:studentTransactions}); 
+    }catch(error){
+        next(new CustomErrors("Error in Loading Student's Payment History", 500))
+    }
+    
 })
 
 module.exports = router;
